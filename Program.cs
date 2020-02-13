a91b2e01b40dfac2513df4ac861a8b75bfb361c4
AwesomeProgrammer <arne@intigriti.be>
2020-02-13 16:05:11 +0100

    Add hello world console

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hashcode2020_problem_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
